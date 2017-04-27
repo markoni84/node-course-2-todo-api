63e97e6763db75c44520156f4fa5a7390808d077
Marko Vukolic <vukolicm@gmail.com>
2017-04-28 01:12:49 +0200

    Init commit

---
+++ playground/app.js
@@ -0,0 +1,7 @@
+// const util = require('./util.js').user;
+const {Users} = require('./util.js');
+
+// var {util} = require('./util.js')
+
+var objKorisnik = new Users()
+console.log(objKorisnik.ime);
---
+++ playground/util.js
@@ -0,0 +1,13 @@
+function Users() {
+  this.ime = 'Marko'
+  //return this;
+}
+
+var user = {
+  name:'marko'
+}
+
+module.exports = {
+  user,
+  Users
+}
