58743d8e590f2980f067f1d1719e4e9ca1dfe715
Marko Vukolic <vukolicm@gmail.com>
2017-05-07 01:31:47 +0200

    Add POST /todos route and refactor mongoose

--- playground/app.js
+++ playground/app.js
@@ -1,7 +1,14 @@
 // const util = require('./util.js').user;
-const {Users} = require('./util.js');
+// const {Users} = require('./util.js');
+var foo = {
+  ime:global.Promise,
 
+  nesto: function() {
+    console.log('a sad'+this.ime);
+  }
+}
+foo.nesto();
 // var {util} = require('./util.js')
 
-var objKorisnik = new Users()
-console.log(objKorisnik.ime);
+//var objKorisnik = new Users()
+// console.log(objKorisnik.ime);
